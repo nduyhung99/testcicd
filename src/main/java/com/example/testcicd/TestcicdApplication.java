7ab5486354a398b069ea4f7eedff207f32c20f0b
MARUSYSVINA\ndhung <ndhung@marusysvina.com>
2026-04-21 17:15:36 +0700

    first commit

---
+++ src/main/java/com/example/testcicd/TestcicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcicdApplication.class, args);
+	}
+
+}
